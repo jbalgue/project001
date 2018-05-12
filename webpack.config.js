41671a08e06994104bddebf666b10c89e8999a37
Jul <jbalgue@users.noreply.github.com>
2018-05-13 01:45:22 +0800

    Vue.js Boilerplate
    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const webpack = require('webpack');
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    mode: 'development',
+
+    entry: [
+        './src/main.js'
+    ],
+
+    devServer: {
+        hot: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new VueLoaderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        }),
+    ]
+};
